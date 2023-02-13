be085d454a2b837cb0db6105a284372507b91a4f
Konstantin Kobliuck <kkobliuk@gmail.com>
2023-02-13 15:36:11 +0300

    feat: add name input and greetings

--- momentum/js/index.js
+++ momentum/js/index.js
@@ -5,9 +5,12 @@ const date = document.querySelector('.date');
 
 function showTime() {
   const currentDate = new Date();
+  const dayTime = getTimeOfDay(currentDate);
+
   time.textContent = currentDate.toLocaleTimeString();
+
   showDate(currentDate);
-  // setTimeout(showTime, 1000);
+  showGreetings(dayTime);
 }
 
 function showDate(d) {
@@ -20,6 +23,49 @@ function showDate(d) {
   date.textContent = (d.toLocaleDateString('en-US', options));
 }
 
-// showTime();
 setInterval(showTime, 1000);
 /*================= 2. greetings ============================= */
+const greetingText = document.querySelector('.greeting__text');
+const personName = document.querySelector('.name');
+const timesOfDay = ['night', 'morning', 'afternoon', 'evening'];
+
+function getTimeOfDay(d) {
+  let hour = d.getHours();
+  let timeOfDay = timesOfDay[Math.floor(hour / 6)];
+  return timeOfDay; 
+}
+
+function showGreetings(time) {
+  if (localStorage.getItem('name')) {
+    greetingText.textContent = `Good ${time},`;
+  } else {
+    greetingText.textContent = `Good ${time}`;
+  }
+}
+
+function setLocalStorage() {
+  localStorage.setItem('name', personName.value);
+}
+
+function getLocalStorage() {
+  let name = localStorage.getItem('name');
+  if (name) {
+    personName.value = name; 
+  }
+}
+
+window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('load', getLocalStorage);
+personName.addEventListener('focus', removeValue);
+personName.addEventListener('keydown', changeFocus);
+
+function removeValue() {
+  personName.value = null;
+}
+
+function changeFocus(e) {
+  if (e.keyCode === 13 || e.keyCode === 27) {
+    e.target.blur();
+  }
+}
+/*================= 3. change img ============================= */
